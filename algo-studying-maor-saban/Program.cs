b9c6d87f5c8edb3ce00a5d659d584ab3c51bebe9
Administrator <Administrator@4115-002>
2024-05-09 18:06:31 +0300

    Add project files.

---
+++ algo-studying-maor-saban/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algo_studying_maor_saban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
