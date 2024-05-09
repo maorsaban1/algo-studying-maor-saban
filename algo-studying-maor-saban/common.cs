e84cbccde017213fac485c71930c4f2d2cc9cb1b
maorsaban1 <maorsaban1@gmail.com>
2024-05-09 19:17:34 +0300

    finish task

---
+++ algo-studying-maor-saban/common.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algo_studying_maor_saban
+{
+    public class common
+    {
+        public static void RunProcess()
+        {
+            // int num = int Console.ReadLine());
+            //if (num == 1)
+            Console.WriteLine("Choose one option:"
+                              + " 1 - World | 2 - Israel | 0 - Exit");
+        }
+
+    }
+}
---
+++ algo-studying-maor-saban/cscommon.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algo_studying_maor_saban
+{
+    class cscommon
+    {
+    }
+}
